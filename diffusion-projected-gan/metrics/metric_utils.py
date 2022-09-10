fixed image scaling issue that was altering FID and generated images

diffusion-projected-gan/metrics/metric_utils.py
@@ -271,35 +271,33 @@ def compute_feature_stats_for_dataset(opts, detector_url, detector_kwargs, rel_l
 
     # get detector
     detector = get_feature_detector(url=detector_url, device=opts.device, num_gpus=opts.num_gpus, rank=opts.rank, verbose=progress.verbose)
-    # we are going to want to imagenet normalize with an imagenet stat generator, get more interpretable FID scores
-    imnet_norm = transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
 
     # Main loop.
     item_subset = [(i * opts.num_gpus + opts.rank) % num_items for i in range((num_items - 1) // opts.num_gpus + 1)]
+    lo, hi = 0, 255
     if opts.rgba:
         for images, _labels in tqdm(
                 torch.utils.data.DataLoader(dataset=dataset, sampler=item_subset, batch_size=batch_size,
                                             **data_loader_kwargs)):
             # separate the img and mask, make mask rgb
-            images = images[:, :-1, :, :]
-            masks = images[:, -1:, :, :].repeat([1, 3, 1, 1])
+            raw_images = torch.empty_like(images[:, :-1, :, :])
+            raw_masks = torch.empty_like(images[:, :-1, :, :])
 
-            # imagenet normalize
-            images = imnet_norm(images.to(torch.float32))
-            masks = imnet_norm(masks.to(torch.float32))
+            # cast the values
+            raw_images[:] = images[:, :-1, :, :][:]
+            raw_masks[:] = images[:, -1:, :, :].repeat([1, 3, 1, 1])[:]
 
-            # scale the images to 0-255
-            for gen_id in range(images.shape[0]):
-                images[gen_id] = ((images[gen_id] - images[gen_id].min()) / (images[gen_id].max() - images[gen_id].min())) * 255
-                masks[gen_id] = ((masks[gen_id] - masks[gen_id].min()) / (masks[gen_id].max() - masks[gen_id].min())) * 255
+            # # scale the images to 0-255
+            # images = (raw_images - lo) * (255 / (hi - lo))
+            # masks = (raw_masks - lo) * (255 / (hi - lo))
 
             # round and clamp as needed
-            images.round().clamp(0, 255).to(torch.uint8)
-            masks.round().clamp(0, 255).to(torch.uint8)
+            raw_images.to(torch.uint8)
+            raw_masks.to(torch.uint8)
 
             with torch.no_grad():
-                img_features = detector(images.to(opts.device), **detector_kwargs)
-                mask_features = detector(masks.to(opts.device), **detector_kwargs)
+                img_features = detector(raw_images.to(opts.device), **detector_kwargs)
+                mask_features = detector(raw_masks.to(opts.device), **detector_kwargs)
 
             stats.append_torch(img_features, num_gpus=opts.num_gpus, rank=opts.rank)
             stats_seg.append_torch(mask_features, num_gpus=opts.num_gpus, rank=opts.rank)
@@ -322,11 +320,8 @@ def compute_feature_stats_for_dataset(opts, detector_url, detector_kwargs, rel_l
         return stats, stats_seg
     else:
         for images, _labels in tqdm(torch.utils.data.DataLoader(dataset=dataset, sampler=item_subset, batch_size=batch_size, **data_loader_kwargs)):
-            if images.shape[1] == 1: # assumes single channel to rgb
+            if images.shape[1] == 1: # assumes single channel to rgb, adjust this all later
                 images = images.repeat([1, 3, 1, 1])
-
-            # imagenet normalize
-            images = imnet_norm(images.to(torch.float32))
 
             # scale the images to 0-255
             for gen_id in range(images.shape[0]):
@@ -380,42 +375,44 @@ def compute_feature_stats_for_generator(opts, detector_url, detector_kwargs, rel
 
     # get detector
     detector = get_feature_detector(url=detector_url, device=opts.device, num_gpus=opts.num_gpus, rank=opts.rank, verbose=progress.verbose)
-    # we are going to want to imagenet normalize with an imagenet stat generator, get more interpretable FID scores
-    imnet_norm = transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
 
     # Main loop.
     # need to specify for rgba so that we can get stats for masks
+    lo, hi = -1, 1
     if opts.rgba:
         while not stats.is_full():
-            images = []
-            masks = []
+            images_full = []
+            masks_full = []
             for _i in range(batch_size // batch_gen):
                 z = torch.randn([batch_gen, G.z_dim], device=opts.device)
                 # img = G(z=z, c=next(c_iter), truncation_psi=0.1, **opts.G_kwargs)
 
                 # get the image outputs
-                raw_img = G(z=z, c=next(c_iter), **opts.G_kwargs)
+                images = G(z=z, c=next(c_iter), **opts.G_kwargs)
 
                 # separate the img and mask, make mask rgb
-                img = raw_img[:, :-1, :, :]
-                mask = raw_img[:, -1:, :, :].repeat([1, 3, 1, 1])
+                raw_images = torch.empty_like(images[:, :-1, :, :])
+                raw_masks = torch.empty_like(images[:, :-1, :, :])
 
-                # imagenet normalize
-                img = imnet_norm(img)
-                mask = imnet_norm(mask)
+                # cast the values
+                raw_images[:] = images[:, :-1, :, :][:]
+                raw_masks[:] = images[:, -1:, :, :].repeat([1, 3, 1, 1])[:]
 
                 # scale the images to 0-255
-                for gen_id in range(img.shape[0]):
-                    img[gen_id] = ((img[gen_id] - img[gen_id].min()) / (img[gen_id].max() - img[gen_id].min())) * 255
-                    mask[gen_id] = ((mask[gen_id] - mask[gen_id].min()) / (mask[gen_id].max() - mask[gen_id].min())) * 255
+                images = (raw_images - lo) * (255 / (hi - lo))
+                masks = (raw_masks - lo) * (255 / (hi - lo))
+
+                # round and clamp as needed
+                images.round().clamp(0, 255).to(torch.uint8)
+                masks.round().clamp(0, 255).to(torch.uint8)
 
                 # append them as needed
-                images.append(img.round().clamp(0, 255).to(torch.uint8))
-                masks.append(mask.round().clamp(0, 255).to(torch.uint8))
+                images_full.append(images)
+                masks_full.append(masks)
 
             # get the input tensors
-            images = torch.cat(images)
-            masks = torch.cat(masks)
+            images = torch.cat(images_full)
+            masks = torch.cat(masks_full)
 
             # get the features
             with torch.no_grad():
@@ -443,7 +440,6 @@ def compute_feature_stats_for_generator(opts, detector_url, detector_kwargs, rel
                 # imagenet normalize
                 if img.shape[1] == 1:
                     img = img.repeat([1, 3, 1, 1])
-                img = imnet_norm(img)
 
                 # scale the images to 0-255
                 for gen_id in range(img.shape[0]):

diffusion-projected-gan/train.py
@@ -103,9 +103,10 @@ def launch_training(c, desc, outdir, dry_run):
             torch.multiprocessing.spawn(fn=subprocess_fn, args=(c, temp_dir), nprocs=c.num_gpus)
 
 
-def init_dataset_kwargs(data, rgba=False, rgba_mode='mean_extract'):
+def init_dataset_kwargs(data, rgba=False, rgba_mode='mean_extract', rgba_mult=1):
     try:
-        dataset_kwargs = dnnlib.EasyDict(class_name='training.dataset.ImageFolderDataset', path=data, use_labels=True, max_size=None, xflip=False, rgba=rgba, rgba_mode=rgba_mode)
+        dataset_kwargs = dnnlib.EasyDict(class_name='training.dataset.ImageFolderDataset', path=data, use_labels=True,
+                                         max_size=None, xflip=False, rgba=rgba, rgba_mode=rgba_mode, rgba_mult=rgba_mult)
         dataset_obj = dnnlib.util.construct_class_by_name(**dataset_kwargs) # Subclass of training.dataset.Dataset.
         dataset_kwargs.resolution = dataset_obj.resolution # Be explicit about resolution.
         dataset_kwargs.use_labels = dataset_obj.has_labels # Be explicit about labels.
@@ -128,7 +129,7 @@ def parse_comma_separated_list(s):
 # Required.
 @click.option('--outdir',       help='Where to save the results', metavar='DIR',                default='/data/pcicales/diffusionGAN')
 @click.option('--cfg',          help='Base configuration',                                      type=click.Choice(['fastgan', 'fastgan_lite', 'stylegan2']), default='fastgan')
-@click.option('--data',         help='Training data', metavar='[ZIP|DIR]',                      type=str, default='/data/public/HULA/GLOM_RGBA_SG2/GLOM_RGBA_SG2_1k.zip')
+@click.option('--data',         help='Training data', metavar='[ZIP|DIR]',                      type=str, default='/data/public/HULA/GLOM_RGBA_SG2/GLOM_RGBA_SG2_FULL.zip')
 @click.option('--gpus',         help='Number of GPUs to use', metavar='INT',                    type=click.IntRange(min=1), default=2)
 @click.option('--batch',        help='Total batch size', metavar='INT',                         type=click.IntRange(min=1), default=32)
 
@@ -141,6 +142,7 @@ def parse_comma_separated_list(s):
 # Segmentation config
 @click.option('--rgba',       help='Whether or not we are generating with mask', metavar='BOOL', type=bool, default=True)
 @click.option('--rgba_mode',  help='How we encode our masks', metavar='STR', type=click.Choice(['mean_extract', 'naive']), default='mean_extract')
+@click.option('--rgba_mult',  help='What multiplier to use on binary mask', metavar='INT', type=int, default=3)
 
 # Optional features.
 @click.option('--cond',         help='Train conditional model', metavar='BOOL',                 type=bool, default=False, show_default=True)
@@ -160,14 +162,13 @@ def parse_comma_separated_list(s):
 @click.option('--metrics',      help='Quality metrics', metavar='[NAME|A,B,C|none]',            type=parse_comma_separated_list, default='fid50k_full', show_default=True)
 @click.option('--kimg',         help='Total training duration', metavar='KIMG',                 type=click.IntRange(min=1), default=50000, show_default=True)
 @click.option('--tick',         help='How often to print progress', metavar='KIMG',             type=click.IntRange(min=1), default=4, show_default=True)
-@click.option('--snap',         help='How often to save snapshots', metavar='TICKS',            type=click.IntRange(min=1), default=1, show_default=True)
+@click.option('--snap',         help='How often to save snapshots', metavar='TICKS',            type=click.IntRange(min=1), default=2, show_default=True)
 @click.option('--seed',         help='Random seed', metavar='INT',                              type=click.IntRange(min=0), default=0, show_default=True)
 @click.option('--fp32',         help='Disable mixed-precision', metavar='BOOL',                 type=bool, default=False, show_default=True)
 @click.option('--nobench',      help='Disable cuDNN benchmarking', metavar='BOOL',              type=bool, default=False, show_default=True)
 @click.option('--workers',      help='DataLoader worker processes', metavar='INT',              type=click.IntRange(min=1), default=3, show_default=True)
 @click.option('-n','--dry-run', help='Print training options and exit',                         is_flag=True)
 @click.option('--restart_every',help='Time interval in seconds to restart code', metavar='INT', type=int, default=9999999, show_default=True)
-
 
 def main(**kwargs):
     # Initialize config.
@@ -181,7 +182,7 @@ def main(**kwargs):
     c.ada_kimg = opts.ada_kimg
 
     # Training set.
-    c.training_set_kwargs, dataset_name = init_dataset_kwargs(data=opts.data, rgba=opts.rgba, rgba_mode=opts.rgba_mode)
+    c.training_set_kwargs, dataset_name = init_dataset_kwargs(data=opts.data, rgba=opts.rgba, rgba_mode=opts.rgba_mode, rgba_mult=opts.rgba_mult)
     if opts.cond and not c.training_set_kwargs.use_labels:
         raise click.ClickException('--cond=True requires labels specified in dataset.json')
     c.training_set_kwargs.use_labels = opts.cond

diffusion-projected-gan/training/dataset.py
@@ -33,7 +33,8 @@ class Dataset(torch.utils.data.Dataset):
         xflip       = False,    # Artificially double the size of the dataset via x-flips. Applied after max_size.
         random_seed = 1,        # Random seed to use when applying max_size.
         rgba = False,
-        rgba_mode = ''
+        rgba_mode = '',
+        rgba_mult = 1
     ):
         self._name = name
         self._raw_shape = list(raw_shape)
@@ -42,6 +43,7 @@ class Dataset(torch.utils.data.Dataset):
         self._label_shape = None
         self._rgba = rgba
         self._rgba_mode = rgba_mode
+        self._rgba_mult = rgba_mult
 
         # Apply max_size.
         self._raw_idx = np.arange(self._raw_shape[0], dtype=np.int64)
@@ -181,6 +183,7 @@ class ImageFolderDataset(Dataset):
         self._zipfile = None
         self._rgba = super_kwargs['rgba']
         self._rgba_mode = super_kwargs['rgba_mode']
+        self._rgba_mult = super_kwargs['rgba_mult']
 
         if os.path.isdir(self._path):
             self._type = 'dir'
@@ -242,7 +245,16 @@ class ImageFolderDataset(Dataset):
                 raise AssertionError('RGBA mode must have RGBA inputs! Image with shape {} invalid!'.format(image.shape))
             if self._rgba_mode == 'mean_extract':
                 # encode the mask using a mean of all pixels, extract the mask from generated images later
-                image[:, :, -1][:] = np.rint(np.mean(image, axis=2)).astype('uint8')[:]
+                # generate the mult mask
+                image = image.astype('float32')
+                image[:, :, -1][:] = (image[:, :, -1] * self._rgba_mult)[:]
+                # get the raw mask, scale
+                raw_mask = np.mean(image, axis=2)
+                lo, hi = 0, ((3 + self._rgba_mult) * 255) / 4
+                raw_mask = (raw_mask - lo) * (255 / (hi - lo))
+                # raw_mask = ((raw_mask - raw_mask.min()) * (1/(raw_mask.max() - raw_mask.min()) * 255))
+                image[:, :, -1][:] = np.rint(raw_mask)[:]
+                image = image.astype('uint8')
                 # debugging purposes
                 # raw_mask = (image[:, :, -1:].astype('float')*4) - np.sum(image[:, :, :-1].astype('float'), axis=2)
                 # mask_quants = np.quantile(raw_mask, [0, 0.5, 1])

diffusion-projected-gan/training/training_loop.py
@@ -14,6 +14,7 @@
 
 import os
 import sys
+from sklearn.cluster import KMeans
 
 # getting the name of the directory
 # where the this file is present.
@@ -95,18 +96,30 @@ def save_image_grid(img, fname, drange, grid_size, rgba=False, rgba_mode=''):
     if rgba:
         # reconstruct the alpha channel as needed
         if rgba_mode == 'mean_extract':
-            # decode the masks
-            raw_mask = (img[:, -1:, :, :] * 4) - np.sum(img[:, :-1, :, :], axis=1)[:, np.newaxis, :, :]
-            # can consider using quantiles later...
-            # mask_quants = [np.quantile(sub_mask, [0, 0.2, 1]) for sub_mask in raw_mask]
+            # decode the masks, make copies to avoid strange outputs
+            raw_mask = np.empty_like(img[:, -1:, :, :])
+            raw_img = np.empty_like(img[:, :-1, :, :])
+            # cast values
+            raw_mask[:] = img[:, -1:, :, :][:]
+            raw_img[:] = img[:, :-1, :, :][:]
 
             # get the binary masks
             for mask_id in range(raw_mask.shape[0]):
-                raw_mask[mask_id][np.where(raw_mask[mask_id] <= 127.5)] = 0
-                raw_mask[mask_id][np.where(raw_mask[mask_id] > 127.5)] = 255
+                simg = raw_img[mask_id, :, :, :]
+                bmask = (raw_mask[mask_id, 0, :, :] * 4) - np.sum(simg, axis=0)
+                out_mask = np.empty_like(bmask)
+                _, mask_grouping = np.histogram(bmask.flatten(), bins=500)
+                try:
+                    mask_k = KMeans(n_clusters=2, random_state=0).fit(mask_grouping.reshape(-1, 1))
+                    out_mask[bmask >= mask_grouping[mask_k.labels_ == mask_k.cluster_centers_.argmax()].min()] = 255
+                    out_mask[bmask < mask_grouping[mask_k.labels_ == mask_k.cluster_centers_.argmax()].min()] = 0
+                    img[mask_id, -1:, :, :][:] = out_mask[:]
+                except:
+                    print('WARNING: Generated masks cant be clustered... if this isnt init, check generator.')
+                    img[mask_id, -1:, :, :][:] = out_mask[:]
 
             # now we get the np arrays as uint8
-            mask = raw_mask.astype('uint8')
+            mask = np.rint(img[:, -1:, :, :]).clip(0, 255).astype(np.uint8)
             img = np.rint(img[:, :-1, :, :]).clip(0, 255).astype(np.uint8)
 
         # naive method just scales the prediction
@@ -300,13 +313,21 @@ def training_loop(
     if rank == 0:
         print('Exporting sample images...')
         grid_size, images, labels = setup_snapshot_image_grid(training_set=training_set)
-        save_image_grid(images, os.path.join(run_dir, 'reals.png'), drange=[0,255], grid_size=grid_size, rgba=training_set_kwargs['rgba'], rgba_mode=training_set_kwargs['rgba_mode'])
+        save_image_grid(images, os.path.join(run_dir, 'reals.png'), drange=[0,255],
+                        grid_size=grid_size, rgba=training_set_kwargs['rgba'],
+                        rgba_mode=training_set_kwargs['rgba_mode'])
 
         grid_z = torch.randn([labels.shape[0], G.z_dim], device=device).split(batch_gpu)
         grid_c = torch.from_numpy(labels).to(device).split(batch_gpu)
-        images = torch.cat([G_ema(z=z, c=c, noise_mode='const').cpu() for z, c in zip(grid_z, grid_c)]).numpy()
 
-        save_image_grid(images, os.path.join(run_dir, 'fakes_init.png'), drange=[-1,1], grid_size=grid_size, rgba=training_set_kwargs['rgba'], rgba_mode=training_set_kwargs['rgba_mode'])
+        if (training_set_kwargs['rgba_mode'] == 'mean_extract') and training_set_kwargs['rgba']:
+            print('Not exporting init fakes when using mean_extract for masks...')
+        else:
+            images = torch.cat([G_ema(z=z, c=c, noise_mode='const').cpu() for z, c in zip(grid_z, grid_c)]).numpy()
+
+            save_image_grid(images, os.path.join(run_dir, 'fakes_init.png'), drange=[-1,1],
+                            grid_size=grid_size, rgba=training_set_kwargs['rgba'],
+                            rgba_mode=training_set_kwargs['rgba_mode'])
 
     # Initialize logs.
     if rank == 0: