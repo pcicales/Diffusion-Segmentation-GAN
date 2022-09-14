8fda1b0fc0b36f51e97680a2db8432a3dd792707
Pietro Cicalese <pietrocicalese134@gmail.com>
2022-09-13 23:14:11 -0500

    Added RGBA functionality to gen_video

--- diffusion-projected-gan/gen_video.py
+++ diffusion-projected-gan/gen_video.py
@@ -21,6 +21,12 @@ import scipy.interpolate
 import torch
 from tqdm import tqdm
 
+import cv2
+
+import matplotlib.pyplot as plt
+
+from sklearn.cluster import KMeans
+
 import legacy
 
 #----------------------------------------------------------------------------
@@ -43,7 +49,9 @@ def layout_grid(img, grid_w=None, grid_h=1, float_to_uint8=True, chw_to_hwc=True
 
 #----------------------------------------------------------------------------
 
-def gen_interp_video(G, mp4: str, seeds, shuffle_seed=None, w_frames=60*4, kind='cubic', grid_dims=(1,1), num_keyframes=None, wraps=2, psi=1, device=torch.device('cuda'), class_idx=None, **video_kwargs):
+def gen_interp_video(G, mp4: str, seeds, shuffle_seed=None, w_frames=60*4, kind='cubic', grid_dims=(1,1),
+                     num_keyframes=None, wraps=2, psi=1, device=torch.device('cuda'), class_idx=None,
+                     rgba=False, rgba_mode='', rgba_mult=1, mask_cutoff=0.4,  **video_kwargs):
     grid_w = grid_dims[0]
     grid_h = grid_dims[1]
 
@@ -95,8 +103,87 @@ def gen_interp_video(G, mp4: str, seeds, shuffle_seed=None, w_frames=60*4, kind=
                 interp = grid[yi][xi]
                 w = torch.from_numpy(interp(frame_idx / w_frames)).to(device).float()
                 img = G.synthesis(w.unsqueeze(0), c=label, noise_mode='const')[0]
-                imgs.append(img)
+
+                # check for segmentation task
+                if rgba:
+                    lo, hi = -1, 1
+                    img = (img - lo) * (255 / (hi - lo))
+
+                    # reconstruct the alpha channel as needed
+                    if rgba_mode == 'mean_extract':
+                        img = img.cpu()
+                        # decode the masks, make copies to avoid strange outputs
+                        raw_mask = np.empty_like(img[-1:, :, :]).squeeze(0)
+                        raw_img = np.empty_like(img[:-1, :, :])
+                        # cast values
+                        raw_mask[:] = img[-1:, :, :][:]
+                        raw_img[:] = img[:-1, :, :][:]
+
+                        # get the binary masks
+                        bmask = (raw_mask * 4) - np.sum(raw_img, axis=0)
+                        out_mask = np.empty_like(bmask)
+                        _, mask_grouping = np.histogram(bmask.flatten(), bins=500)
+                        try:
+                            mask_k = KMeans(n_clusters=2, random_state=0).fit(mask_grouping.reshape(-1, 1))
+                            out_mask[
+                                bmask >= mask_grouping[mask_k.labels_ == mask_k.cluster_centers_.argmax()].min()] = 255
+                            out_mask[
+                                bmask < mask_grouping[mask_k.labels_ == mask_k.cluster_centers_.argmax()].min()] = 0
+                            img[-1:, :, :][:] = torch.from_numpy(out_mask)[:]
+                        except:
+                            print('WARNING: Generated masks cant be clustered... if this isnt init, check generator.')
+                            continue
+
+                        # Convert the image
+                        img = np.rint(img.permute(1, 2, 0).numpy()).astype('uint8')
+
+                        # we now need to clean the mask as needed
+                        # 1) morphological transformation (eliminate small islands)
+                        se1 = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5))
+                        se2 = cv2.getStructuringElement(cv2.MORPH_RECT, (2, 2))
+                        mask = cv2.morphologyEx(img[:, :, -1], cv2.MORPH_CLOSE, se1)
+                        mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, se2)
+                        img[..., -1] = mask
+
+                        # 2) flood fill (fill in any holes)
+                        # Copy the thresholded image.
+                        im_floodfill = img[..., -1].copy()
+
+                        # Mask used to flood filling.
+                        # Notice the size needs to be 2 pixels than the image.
+                        h, w = img[..., -1].shape[:2]
+                        mask = np.zeros((h + 2, w + 2), np.uint8)
+
+                        # Floodfill from point (0, 0)
+                        cv2.floodFill(im_floodfill, mask, (0, 0), 255)
+
+                        # Invert floodfilled image
+                        im_floodfill_inv = cv2.bitwise_not(im_floodfill)
+
+                        # Combine the two images to get the foreground.
+                        img[..., -1] = img[..., -1] | im_floodfill_inv
+
+                        # 3) size filtering (remove any contours that are too small)
+                        contour_stack = []
+                        contours, _ = cv2.findContours(img[..., -1], cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+                        mask = np.zeros_like(img[..., -1])
+                        for contour in contours:
+                            area = cv2.contourArea(contour)
+                            if area > mask_cutoff:
+                                contour_stack.append(contour)
+
+                        # draw the contours onto the image
+                        img = img.copy()[:, :, :-1]
+                        img = cv2.drawContours(img.astype(np.uint8), contour_stack, -1, (255, 0, 0), 3)
+
+                        # append
+                        img = np.interp(img, (0, 255), (-1, +1))
+                        imgs.append(torch.from_numpy(img).permute(2, 0, 1).float().to(device))
+                else:
+                    imgs.append(img)
+
         video_out.append_data(layout_grid(torch.stack(imgs), grid_w=grid_w, grid_h=grid_h))
+
     video_out.close()
 
 #----------------------------------------------------------------------------
@@ -135,15 +222,23 @@ def parse_tuple(s: Union[str, Tuple[int,int]]) -> Tuple[int, int]:
 #----------------------------------------------------------------------------
 
 @click.command()
-@click.option('--network', 'network_pkl', help='Network pickle filename', required=True)
-@click.option('--seeds', type=parse_range, help='List of random seeds', required=True)
+@click.option('--network', 'network_pkl', help='Network pickle filename', default='/data/pcicales/diffusionGAN/00000-fastgan-GLOM_RGBA_SG2_FULL-gpus2-batch32-d_pos-first-noise_sd-0.5-target0.45-ada_kimg100/best_model.pkl')
+@click.option('--seeds', type=parse_range, help='List of random seeds (must be divisible by grid W*H)', default='0-59')
 @click.option('--shuffle-seed', type=int, help='Random seed to use for shuffling seed order', default=None)
-@click.option('--grid', type=parse_tuple, help='Grid width/height, e.g. \'4x3\' (default: 1x1)', default=(1,1))
+@click.option('--grid', type=parse_tuple, help='Grid width/height, e.g. \'4x3\' (default: 1x1)', default=(4,3))
 @click.option('--num-keyframes', type=int, help='Number of seeds to interpolate through.  If not specified, determine based on the length of the seeds array given by --seeds.', default=None)
 @click.option('--w-frames', type=int, help='Number of frames to interpolate between latents', default=120)
 @click.option('--trunc', 'truncation_psi', type=float, help='Truncation psi', default=1, show_default=True)
-@click.option('--output', help='Output .mp4 filename', type=str, required=True, metavar='FILE')
+@click.option('--output', help='Output .mp4 filename', type=str, default='/data/public/HULA/GEN_GLOM_RGBA_VIDEOS/{}.mp4', metavar='FILE')
 @click.option('--class', 'class_idx', type=int, help='Class label (unconditional if not specified)')
+
+# Segmentation config
+@click.option('--rgba',       help='Whether or not we are generating with mask', metavar='BOOL', type=bool, default=True)
+@click.option('--rgba_mode',  help='How we encode our masks', metavar='STR', type=click.Choice(['mean_extract', 'naive']), default='mean_extract')
+@click.option('--rgba_mult',  help='What multiplier to use on binary mask', metavar='INT', type=int, default=3)
+@click.option('--mask_cutoff',  help='Pixel filtering to remove noisy contours', metavar='INT', type=int, default=200)
+
+
 def generate_images(
     network_pkl: str,
     seeds: List[int],
@@ -154,6 +249,10 @@ def generate_images(
     w_frames: int,
     output: str,
     class_idx: Optional[int],
+    rgba: Optional[bool],
+    rgba_mode: str,
+    rgba_mult: Optional[int],
+    mask_cutoff: Optional[int]
 ):
     """Render a latent vector interpolation video.
 
@@ -179,10 +278,21 @@ def generate_images(
 
     print('Loading networks from "%s"...' % network_pkl)
     device = torch.device('cuda')
+    if rgba:
+        if '{}' not in output:
+            raise AssertionError('Output must have `{}` in file name to identify the video!')
+
+        if '/' in output:
+            os.makedirs(output.split('{}', 1)[0], exist_ok=True)
+
+        output = output.format(network_pkl.split('/')[-2])
+
     with dnnlib.util.open_url(network_pkl) as f:
         G = legacy.load_network_pkl(f)['G_ema'].to(device) # type: ignore
 
-    gen_interp_video(G=G, mp4=output, bitrate='12M', grid_dims=grid, num_keyframes=num_keyframes, w_frames=w_frames, seeds=seeds, shuffle_seed=shuffle_seed, psi=truncation_psi, class_idx=class_idx)
+    gen_interp_video(G=G, mp4=output, bitrate='12M', grid_dims=grid, num_keyframes=num_keyframes, w_frames=w_frames,
+                     seeds=seeds, shuffle_seed=shuffle_seed, psi=truncation_psi, class_idx=class_idx, rgba=rgba, rgba_mode=rgba_mode,
+                     rgba_mult=rgba_mult, mask_cutoff=mask_cutoff)
 
 #----------------------------------------------------------------------------
 
